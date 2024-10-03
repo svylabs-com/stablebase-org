b3116b5a5202348c74016e5a4756d70e9c9e992e
rohitbharti279 <rohitbharti279@gmail.com>
2024-10-03 11:44:31 +0530

    disable scrolling on mobile devices in What section

--- src/Components/Overview/What.tsx
+++ src/Components/Overview/What.tsx
@@ -9,47 +9,17 @@ const What: React.FC = () => {
             </h1>
             <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
                 {/* Left Side Image (Visible as background on mobile) */}
-                <div className="relative w-full lg:w-1/2 lg:bg-gray-400 lg:p-6 rounded-md">
+                <div className="hidden lg:block w-full lg:w-1/2 lg:bg-gray-400 lg:p-6 rounded-md">
                     <img
                         src={protocolImage}
                         alt="Protocol Overview"
                         className="w-full h-full object-cover opacity-40 md:h-[30rem] lg:static lg:shadow-lg lg:rounded-md lg:opacity-100"
                     />
-
-                    {/* Overlay Content for mobile */}
-                    <div className="absolute inset-0 bg-black bg-opacity-50 lg:hidden overflow-y-auto max-h-full p-6 flex flex-col justify-center items-center text-center">
-                        <div className="overflow-y-auto max-h-[80vh] w-full"> {/* Ensuring scrollable area */}
-                            <h2 className="text-lg md:text-3xl font-semibold mb-4  text-violet-500">
-                                {/* Stablebase Protocol Overview */}
-                                WHAT IS STABLEBASE
-                            </h2>
-                            <h4 className="text-lg md:text-3xl font-semibold mb-4 text-violet-300">
-                            CDP based Stablecoin Protocol
-                                </h4>
-                            <p className="text-base leading-relaxed mb-4 text-white">
-                                StableBase is a decentralized CDP based protocol that allows users to mint a USD-pegged stablecoin by depositing ETH or native assets as collateral. 
-                                The protocol aims to improve borrowing experience for both large and small users by providing an alternative mechanism to traditional interest rate
-                                based CDP protocols.
-                            </p>
-                            <h4 className="text-md md:text-xl font-semibold mb-6 text-violet-300">
-                                Caters to Different Risk Profiles
-                            </h4>
-                            <p className="text-base leading-relaxed mb-4 text-white">
-                                For users that can tolerate risk and have advanced capabilities, the protocol offers a 0% interest rate on loans, and in return expects the users to actively manage their positions.
-                            </p>
-                            <p className="text-base leading-relaxed mb-4 text-white">
-                                For regular users that are risk averse, the protocol offers predictable borrowing terms for a pre-paid fee.
-                            </p>
-                            <button className="px-6 py-3 mt-4 bg-violet-600 text-white font-semibold rounded-lg shadow-lg hover:bg-violet-700 transition ease-in-out duration-300">
-                                Learn More
-                            </button>
-                        </div>
-                    </div>
                 </div>
 
                 {/* Right Side Content for Desktop */}
-                <div className="hidden lg:block lg:w-1/2 lg:pl-10">
-                    <h2 className="text-3xl font-semibold mb-4  text-violet-500" >
+                <div className="lg:w-1/2 lg:pl-10  text-center lg:text-left">
+                    <h2 className="text-lg md:text-3xl font-semibold mb-4  text-violet-500" >
                         WHAT IS STABLEBASE
                     </h2>
                     <h4 className="text-md md:text-xl font-semibold mb-6 text-violet-300">
