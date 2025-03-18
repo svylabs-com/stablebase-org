2e32d8d52a934934c48c6bd43d4642b3e3b4269a
rohitbharti279 <rohitbharti279@gmail.com>
2025-03-18 17:21:41 +0530

    update background image and add gradient to stablebase

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,9 +7,9 @@ import Team from "./Team";
 import Timeline from "./Timeline";
 // import ContactUs from './ContactUs';
 import Footer from './Footer';
-import universe from "../assets/svg/future2.svg";
+// import universe from "../assets/svg/future2.svg";
+import universe from "../assets/photos/hero-backgroung.jpg";
 import coin from "../assets/svg/dfid.svg";
-//import universe from "../assets/svg/bannerz.svg";
 
 const Home: React.FC = () => {
   return (
@@ -19,8 +19,11 @@ const Home: React.FC = () => {
         style={{ backgroundImage: `url(${universe})` }}
       >
         <div className="flex flex-col gap-4 text-center md:text-left mx-auto">
-          <h1 className="text-3xl sm:text-4xl md:text-6xl font-black tracking-wide text-white">STABLEBASE</h1>
-          <h2 className="text-sm sm:text-base md:text-lg text-[#CCCCCC]">A marketdriven stablecoin protocol with a simple pay as you go pricing</h2>
+          {/* <h1 className="text-3xl sm:text-4xl md:text-6xl font-black tracking-wide text-white">STABLEBASE</h1> */}
+          <h1 className="text-3xl sm:text-4xl md:text-6xl font-black tracking-wide bg-gradient-to-r from-gray-300 via-gray-500 to-white bg-clip-text text-transparent">
+            STABLEBASE
+          </h1>
+          <h2 className="text-sm sm:text-base md:text-lg text-">A marketdriven stablecoin protocol with a simple pay as you go pricing</h2>
           <div className="flex justify-center md:justify-start">
             {/* <a
               className="px-4 sm:px-6 py-2 sm:py-3 bg-yellow-600 text-[#161619] font-semibold rounded-lg shadow-lg hover:bg-yellow-500 hover:text-black transition duration-300 ease-in-out text-xs sm:text-base"
@@ -28,7 +31,7 @@ const Home: React.FC = () => {
             >
               Get started with whitepaper
             </a> */}
-             <a
+            <a
               className="px-4 sm:px-6 py-2 sm:py-3 bg-gradient-to-b from-[#FFD572] to-[#FEBD38] text-[#161619] font-semibold rounded-lg shadow-[0px_10px_20px_0px_#FBCC6440] hover:bg-yellow-500 hover:text-black transition duration-300 ease-in-out text-xs sm:text-base"
               href='https://github.com/svylabs/stablebase/blob/main/WHITEPAPER.md'
             >
